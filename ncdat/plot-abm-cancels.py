e383b3299e13597aef2c96556a345274d63fe832
Bhaskar Mookerji <mookerji@gmail.com>
2020-10-20 15:41:17 -0700

    Updates to plotting script

--- ncdat/plot-abm-cancels.py
+++ ncdat/plot-abm-cancels.py
@@ -13,13 +13,13 @@ from shapefiles import load_shapefile_counties, to_kml, read_zip_codes
 
 gpd.io.file.fiona.drvsupport.supported_drivers['KML'] = 'rw'
 
+
 @click.command()
 @click.option('-f', '--filename', type=str)
 @click.option('-z', '--zip-file', type=click.Path(exists=True))
 @click.option('-c', '--county', type=str, default=None)
 @click.option('-p', '--party', type=str, default='Democrats')
 def main(filename, zip_file, county, party):
-
     # Get absentee
     absentee = read_absentee_voter_file(filename)
     if county:
@@ -33,46 +33,26 @@ def main(filename, zip_file, county, party):
     absentee \
       = absentee[~absentee.ballot_rtn_status.str.contains('ACCEPTED') & (absentee.party == party)]
 
-    #absentee.dropna(subset=['ballot_mail_street_address'], inplace=True)
-    # address \
-    #   = absentee.ballot_mail_street_address.apply(lambda s: s if s.rfind('#') == -1 else s[:s.rfind('#')].rstrip())
-    # absentee['location'] \
-    #   = address + ', ' + absentee.ballot_mail_city + ', ' +  absentee.ballot_mail_state + " " + absentee.ballot_mail_zip.round().astype(str)
-    # try:
-    #     absentee['precinct'] = absentee.precinct_desc.str.lstrip('PCT').dropna().astype(int)
-    # except:
-    #     absentee['precinct'] = absentee.precinct_desc
-    # absentee.index.name = 'row_number'
-
-    # # Output
-    # cols = ['race', 'age_group', 'gender', 'location', 'ballot_rtn_status']
-    # absentee[cols].to_csv(f'limbo/{prefix}_rejected_all.csv')
-
-    # Precints
-
-    # precincts = gpd.read_file('limbo/voter_precinct/Voter_Precinct.shp')
-    # precincts = precincts[['precno', 'geometry']]
-    # precincts['name'] = precincts['precno']
-    # precincts.set_index('precno', inplace=True)
-    # precincts['count'] = absentee.groupby(by='precinct').county_desc.count()
-    # precincts.dropna(subset=['count'], inplace=True)
-    # bins = [0, 2, 10, 20]
-    # precincts['quantile'] \
-    #   = pd.cut(precincts['count'], bins, duplicates='drop').astype('str')
-    # cols = ['name', 'count', 'quantile', 'geometry']
-    # to_kml(precincts[cols], f'limbo/{prefix}_rejected_precincts.kml')
+    reasons = {}
+    for zip_code, group in absentee.groupby(by='voter_zip'):
+        reasons[zip_code] = group.groupby(
+            by='ballot_rtn_status').county_desc.count()
+    reasons = pd.DataFrame(reasons).T.fillna(0)
 
     # Zipcodes
     count_by_zip_code = absentee.groupby(by='voter_zip').county_desc.count()
     zips = read_zip_codes(zip_file)
-    zips['count'] = count_by_zip_code
-    zips.dropna(subset=['count'], inplace=True)
+    zips['total'] = count_by_zip_code
+    zips.dropna(subset=['total'], inplace=True)
     bins = [0, 5, 20, 40, 60, 200]
     zips['quantile'] \
-      = pd.cut(zips['count'], bins, duplicates='drop').astype('str')
+      = pd.cut(zips['total'], bins, duplicates='drop').astype('str')
+
+    zips = zips.join(reasons).fillna(0)
     ts = pd.Timestamp.now().isoformat()
     prefix = party.lower()
-    to_kml(zips[['count', 'quantile', 'geometry']], f'limbo/{prefix}_rejected_zip_{ts}.kml')
+    out = zips[['total', 'quantile', 'geometry'] +list(reasons.columns.values)]
+    to_kml(out, f'limbo/{prefix}_rejected_zip_{ts}.kml')
 
 
 if __name__ == '__main__':
