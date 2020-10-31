b2cd7f6b2fb6e05abb8afe0caea4230e5130252d
Bhaskar Mookerji <mookerji@gmail.com>
2020-10-30 22:30:33 -0700

    Fixed images

--- scripts/make-data.sh
+++ scripts/make-data.sh
@@ -1,15 +1,15 @@
 #! /bin/env bash
 
 ZIP_FILE=data/ZIP_Code_Tabulation_Areas-shp/ZIP_Code_Tabulation_Areas.shp
-VOTE_FILE=limbo/absentee_20201103-stripped.csv
+ABM_FILE=limbo/absentee_20201103-stripped.csv
 
 mkdir -p limbo/
 
-python3 ncdat/plot_abm_cancels.py --zip-file "$ZIP_FILE" --filename "$VOTE_FILE" --party Democrats
-python3 ncdat/plot_abm_cancels.py --zip-file "$ZIP_FILE" --filename "$VOTE_FILE" --party Unaffiliated
+python3 ncdat/plot_abm_cancels.py --zip-file "$ZIP_FILE" --filename "$ABM_FILE" --party Democrats
+python3 ncdat/plot_abm_cancels.py --zip-file "$ZIP_FILE" --filename "$ABM_FILE" --party Unaffiliated
 
 python3 ncdat/one_stop_locations.py
 
-python3 ncdat/plot_county_one_stop.py --filename "$VOTE_FILE"
-python3 ncdat/plot_abm_latency.py --filename "$VOTE_FILE"
-python3 ncdat/plot_accepted.py  --filename "$VOTE_FILE"
+python3 ncdat/plot_county_one_stop.py --filename "$ABM_FILE"
+python3 ncdat/plot_abm_latency.py --filename "$ABM_FILE"
+python3 ncdat/plot_accepted.py  --filename "$ABM_FILE"
